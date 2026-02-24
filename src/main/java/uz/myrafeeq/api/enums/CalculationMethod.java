9b51f67f2e1b9c650fcfd4c62d3a5f44d23e06d6
shkhdev <dev.shokhrukh.tashpulatov@gmail.com>
2026-02-24 23:01:10 +0500

    init

--- src/main/java/uz/myrafeeq/api/enums/CalculationMethod.java
+++ src/main/java/uz/myrafeeq/api/enums/CalculationMethod.java
@@ -11,15 +11,36 @@ public enum CalculationMethod {
   DUBAI(com.batoulapps.adhan.CalculationMethod.DUBAI),
   QATAR(com.batoulapps.adhan.CalculationMethod.QATAR),
   KUWAIT(com.batoulapps.adhan.CalculationMethod.KUWAIT),
-  SINGAPORE(com.batoulapps.adhan.CalculationMethod.SINGAPORE);
+  SINGAPORE(com.batoulapps.adhan.CalculationMethod.SINGAPORE),
+  MBOUZ(15.5, 15.5, 3);
 
   private final com.batoulapps.adhan.CalculationMethod adhanMethod;
+  private final double customFajrAngle;
+  private final double customIshaAngle;
+  private final int maghribAdjustment;
 
   CalculationMethod(com.batoulapps.adhan.CalculationMethod adhanMethod) {
     this.adhanMethod = adhanMethod;
+    this.customFajrAngle = 0;
+    this.customIshaAngle = 0;
+    this.maghribAdjustment = 0;
+  }
+
+  CalculationMethod(double fajrAngle, double ishaAngle, int maghribAdjustment) {
+    this.adhanMethod = null;
+    this.customFajrAngle = fajrAngle;
+    this.customIshaAngle = ishaAngle;
+    this.maghribAdjustment = maghribAdjustment;
   }
 
   public CalculationParameters getParameters() {
-    return adhanMethod.getParameters();
+    if (adhanMethod != null) {
+      return adhanMethod.getParameters();
+    }
+    CalculationParameters params = com.batoulapps.adhan.CalculationMethod.OTHER.getParameters();
+    params.fajrAngle = customFajrAngle;
+    params.ishaAngle = customIshaAngle;
+    params.adjustments.maghrib = maghribAdjustment;
+    return params;
   }
 }
